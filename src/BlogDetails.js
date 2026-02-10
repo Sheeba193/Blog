d25ea66458633a73dc7f2b4878eb7d2efce6d66e
Sheeba193 <bethkerubo193@gmail.com>
2026-02-10 15:01:22 +0300

    deleteing blogs using DELETE request to fetch api

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,10 +1,20 @@
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory, useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const { id } = useParams();  // Using useParams to access the dynamic route parameters (e.g., blog ID)
+    
     const {data:blog, error, isPending} = useFetch ('http://localhost:8000/blogs/' + id);  // Fetching the blog details using the blog ID from the URL   
+    
+    const history = useHistory();
 
+    const handleClick = () => {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(() => {
+            history.push('/');
+        })
+    };
 
     return ( 
         <div className="blog-details">
@@ -15,6 +25,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Written by {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleClick}>delete</button>
                 </article>
             )}
         </div>
